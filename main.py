45326ac70f41612a64d9a890242f302ce8d37f9b
Darkestlegends <111674431+Darkestlegends@users.noreply.github.com>
2023-12-11 16:40:48 +0000

    ._.

--- main.py
+++ main.py
@@ -94,40 +94,108 @@ async def search(ctx, query=None, mode='free'):
                         "Please provide a query to get started!",
             color=0x3498db
         )
-        message = await ctx.send(embed=initial_embed)
-        await message.add_reaction("⬇️")  # Add the download emoji reaction
+        await ctx.send(embed=initial_embed)
         return
 
-    # ... (rest of the search command)
+    page = 1
+    results_per_page = 3
+    message = None
 
-# Function to handle the search result download action
-async def handle_search_download(ctx, game_name, scripts):
-    await handle_download(ctx, game_name, scripts)
+    while True:
+        api_url = f"https://scriptblox.com/api/script/search?q={query}&mode={mode}&page={page}"
+        response = requests.get(api_url)
+        data = response.json()
 
-@bot.event
-async def on_reaction_add(reaction, user):
-    if user == bot.user:  # Ignore reactions from the bot itself
-        return
+        if "result" in data and "scripts" in data["result"]:
+            scripts = data["result"]["scripts"]
 
-    message = reaction.message
+            if not scripts:
+                await ctx.send("No scripts found!")
+                break
 
-    if message.author == bot.user:  # Check if the message is sent by the bot
-        if str(reaction.emoji) == "⬇️":  # Check for the download emoji reaction
-            query = message.embeds[0].title.split()[-1]  # Extract the query from the embed title
-            await message.delete()  # Remove the original search message
-            await search_and_download(message.channel, query)
+            embed_fields = []
+            start_index = (page - 1) * results_per_page
+            end_index = start_index + results_per_page
+            current_page_scripts = scripts[start_index:end_index]
 
-# Function to perform search and initiate download
-async def search_and_download(channel, query):
-    api_url = f"https://scriptblox.com/api/script/search?q={query}"
-    response = requests.get(api_url)
-    data = response.json()
+            for script in current_page_scripts:
+                game_name = script["game"]["name"]
+                title = script["title"]
+                script_type = script["scriptType"]
+                script_content = script["script"]
+                views = script["views"]
+                verified = script["verified"]
+                has_key = script.get("key", False)
+                created_at = script["createdAt"]
+                updated_at = script["updatedAt"]
 
-    if "result" in data and "scripts" in data["result"]:
-        scripts = data["result"]["scripts"]
-        await handle_search_download(channel, query, scripts)
-    else:
-        await channel.send(f"No scripts found for the query: {query}")
+                if script_type == "free":
+                    paid_or_free = "Free 💰"
+                else:
+                    paid_or_free = "Paid 💲"
+
+                views_emoji = "👀"
+                verified_emoji = "✅" if verified else "❌"
+                key_emoji = "🔑" if has_key else "🚫"
+
+                max_content_length = 1000
+
+                if len(script_content) > max_content_length:
+                    truncated_script_content = script_content[:max_content_length - 3] + "..."
+                else:
+                    truncated_script_content = script_content
+
+                field_value = (
+                    f"Game: {game_name} {views_emoji}\n"
+                    f"Script Type: {paid_or_free} {verified_emoji}\n"
+                    f"Views: {views}\n"
+                    f"Verified: {verified_emoji}\n"
+                    f"Key Required: {key_emoji}\n"
+                    f"Created At: {created_at}\n"
+                    f"Updated At: {updated_at}\n"
+                    f"```lua\n{truncated_script_content}\n```"
+                )
+
+                embed_fields.append((title, field_value))
+
+            embed = discord.Embed(title=f"Search Results (Page {page}/{data['result']['totalPages']}", color=0x27ae60)
+
+            for field_name, field_value in embed_fields:
+                embed.add_field(name=field_name, value=field_value, inline=False)
+
+            if message:
+                await message.edit(embed=embed)
+            else:
+                message = await ctx.send(embed=embed)
+
+            if page > 1:
+                await message.add_reaction("⬅️")
+
+            if page < data["result"]["totalPages"]:
+                await message.add_reaction("➡️")
+
+            def check(reaction, user):
+                return user == ctx.author and str(reaction.emoji) in ["⬅️", "➡️"]
+
+            try:
+                reaction, _ = await bot.wait_for("reaction_add", check=check, timeout=30.0)
+
+                if str(reaction.emoji) == "⬅️" and page > 1:
+                    page -= 1
+                elif str(reaction.emoji) == "➡️" and page < data["result"]["totalPages"]:
+                    page += 1
+
+                await message.clear_reactions()
+
+                embed.title = f"Search Results - Page {page}/{data['result']['totalPages']})"
+                await message.edit(embed=embed)
+
+            except asyncio.TimeoutError:
+                break
+
+        else:
+            await ctx.send("No scripts found!")
+            break
 
 @bot.command()
 async def download(ctx, game_name=None):
